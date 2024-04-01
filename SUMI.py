7dd19f982b24b1eb41bea855c5147216d2f965c3
Styromaniac <43807387+styromaniac@users.noreply.github.com>
2024-04-01 11:23:10 -0400

    Update SUMI.py

--- SUMI.py
+++ SUMI.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import subprocess
+import platform
 import requests
 import shutil
 import sys
 import os
 import gi
-import platform
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib, Gdk
